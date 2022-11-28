ff9e01a5f38eb4fec8d3e323a4b4dca9a29e1c7a
Ded Wildfield <wildfielded@gmail.com>
2022-11-28 10:33:20 +0800

    Workday upd2 28-11-20022

---
+++ Test-hh2/creds.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+DBNAME = 'project2sql'
+USER = 'dbuser'
+PASSWORD = 'qwerty1234'
+HOST = '127.0.0.10'
+PORT = 5432
+
+#####=====----- THE END -----=====#########################################
