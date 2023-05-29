a26e7c5f93bd3f5d2cd5e22824621f9539cf491e
Ded Wildfield <wildfielded@wildfielded.site>
2023-05-29 11:44:42 +0800

    HH2 clean1 29-05-2023

--- Test-hh2/creds.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-DBNAME = 'project2sql'
-USER = 'dbuser'
-PASSWORD = 'qwerty1234'
-HOST = '127.0.0.10'
-PORT = 5432
-
-#####=====----- THE END -----=====#########################################
